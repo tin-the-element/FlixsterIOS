5b0f9108fa9b12c93723a6966f98349ca69eb0c5
tin-the-element <trungtin.luu@gmail.com>
2021-01-09 23:56:16 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Tin Luu on 1/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
